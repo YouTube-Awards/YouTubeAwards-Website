135ae4d45c5f5e3767d0fd4b5f86a161dd05302b
foxy pirate cove / Fnaf <108185011+foxypiratecove37350@users.noreply.github.com>
2024-11-12 01:04:51 +0000

    Better event start timer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,9 @@
 <script setup>
 
-import { ref, computed } from 'vue'
-
-const event_date = ref(new Date("2024-11-15"))
-const time_delta = computed(() => {
-    return new Date(event_date - Date.now())
-})
+import EventStartTimer from './components/EventStartTimer.vue';
 
 </script>
 
 <template>
-	Event starts in:<br>
-    {{ time_delta.getHours() }}h {{ time_delta.getMinutes() }}min {{ time_delta.getSeconds() }}s
-    {{ new Date(event_date - Date.now()) }}
+	<EventStartTimer/>
 </template>
---
+++ src/views/components/EventStartTimer.vue
@@ -0,0 +1,85 @@
+<script setup>
+
+import { onMounted, onBeforeUnmount, ref } from 'vue'
+
+const event_date = new Date('2024-11-14T22:00:00Z') // Sample date
+
+const update_time_delta = () => {
+    let td = new Date(event_date - new Date())
+    return td
+}
+
+let time_delta = update_time_delta()
+
+const months = ref(time_delta.getMonth())
+const days = ref(time_delta.getDate() - 1)
+const hours = ref(time_delta.getHours() - 1)
+const minutes = ref(time_delta.getMonth())
+const seconds = ref(time_delta.getSeconds())
+
+onMounted(() => {
+    const interval = setInterval(() => {
+        time_delta = update_time_delta()
+        months.value = time_delta.getMonth()
+        days.value = time_delta.getDate() - 1
+        hours.value = time_delta.getHours() - 1
+        minutes.value = time_delta.getMinutes()
+        seconds.value = time_delta.getSeconds()
+    }, 1000)
+
+    onBeforeUnmount(() => {
+        clearInterval(interval)
+    })
+})
+
+</script>
+
+<template>
+
+<section>
+    <h1>The event starts in</h1>
+    <div class="months"><span>Months</span>{{ months }}</div>
+    <div class="days"><span>Days</span>{{ days }}</div>
+    <div class="hours"><span>Hours</span>{{ hours }}</div>
+    <div class="minutes"><span>Minutes</span>{{ minutes }}</div>
+    <div class="seconds"><span>Seconds</span>{{ seconds }}</div>
+</section>
+
+</template>
+
+<style scoped>
+
+section {
+    width: calc(100vw - 2 * 1.5rem);
+    margin: calc(33vh - var(--header-height)) 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    gap: 1rem;
+    padding: 0 2rem;
+}
+
+section > h1 {
+    grid-column: 1 / -1;
+    text-align: center;
+}
+
+section > div {
+    background-color: #222;
+    box-shadow: 0 0 12px #0008;
+    border-radius: 1rem;
+    padding: 1rem 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    font-weight: 600;
+    text-align: center;
+    flex-direction: column;
+    gap: 0.5rem;
+}
+
+section > div > span {
+    font-size: 1.25rem;
+}
+
+</style>
